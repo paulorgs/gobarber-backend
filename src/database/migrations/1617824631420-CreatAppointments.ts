08c26486a38ad9fe310a4a858b956efd2c98333d
Paulo Gomes <paulorgs.flores@gmail.com>
2021-04-08 22:59:27 -0300

    setup Appointments repository and refactor persistence logic

--- src/database/migrations/1617824631420-CreatAppointments.ts
+++ src/database/migrations/1617824631420-CreatAppointments.ts
@@ -12,6 +12,7 @@ export default class CreatAppointments1617824631420
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
